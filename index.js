135788a46384f8e4f98f9276657939ec2d91741f
NAJEEBKHAN123 <najeebkhanlaku.com>
2024-09-29 14:17:09 +0500

    add home page

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express')
 const app = express()
 const PORT = 3000;
 
+app.get('/', (req, res) =>{
+    res.status(200).json({message: 'Fetching all users on / path'})
+})
 app.get('/api/users', (req, res) =>{
     res.status(200).json({message: 'Fetching all users'})
 })
