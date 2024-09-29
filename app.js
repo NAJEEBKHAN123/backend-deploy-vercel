ccea0c39813d11d12e10ecfa5014c0d7dfb5a7e4
NAJEEBKHAN123 <najeebkhanlaku.com>
2024-09-29 14:23:21 +0500

    chnge file name

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = 3000;
 
 app.get('/', (req, res) =>{
-    res.status(200).json({message: 'Fetching all users on / pathj'})
+    res.status(200).json({message: 'Fetching all users on / path'})
 })
 app.get('/api/users', (req, res) =>{
     res.status(200).json({message: 'Fetching all users'})
