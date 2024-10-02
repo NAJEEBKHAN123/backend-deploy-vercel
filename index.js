b6fb7a6596e0e5ef003c35fafcff18850095f86e
NAJEEBKHAN123 <najeebkhanlaku.com>
2024-10-02 15:00:39 +0500

    update code

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express')
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const app = express()
-const PORT = 3000;
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Fetching all users on / path' });
+});
 
-app.get('/', (req, res) =>{
-    res.status(200).json({message: 'Fetching all users on / path'})
-})
-app.get('/api/users', (req, res) =>{
-    res.status(200).json({message: 'Fetching all users'})
-})
+app.get('/api/users', (req, res) => {
+  res.status(200).json({ message: 'Fetching all users' });
+});
 
-app.listen(PORT, () =>{
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
